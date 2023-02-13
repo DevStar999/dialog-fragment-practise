426f0939312da5dd93d4b4d9b3f99d547cc25afb
DevStar999 <wolverinerage99@gmail.com>
2023-02-13 15:35:59 +0530

    Completed code to implement a basic DialogFragment with custom entry and exit animations

--- app/src/main/java/com/example/dialogfragmentpractise/MainActivity.java
+++ app/src/main/java/com/example/dialogfragmentpractise/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.dialogfragmentpractise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyCustomDialog.OnInputListener {
+    private static final String TAG = "MainActivity";
+
+    //widgets
+    private Button mOpenDialog;
+    public TextView mInputDisplay;
+
+    //vars
+    public String mInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mOpenDialog = findViewById(R.id.open_dialog);
+        mInputDisplay = findViewById(R.id.input_display);
+
+        mOpenDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: opening dialog.");
+
+                MyCustomDialog dialog = new MyCustomDialog();
+                dialog.show(getSupportFragmentManager(), "MyCustomDialog");
+            }
+        });
+    }
+
+    @Override
+    public void sendInput(String input) {
+        Log.d(TAG, "sendInput: got the input: " + input);
+
+        // mInputDisplay.setText(input);
+        mInput = input;
+
+        setInputToTextView();
+    }
+
+    private void setInputToTextView(){
+        mInputDisplay.setText(mInput);
     }
 }
---
+++ app/src/main/java/com/example/dialogfragmentpractise/MyCustomDialog.java
@@ -0,0 +1,85 @@
+package com.example.dialogfragmentpractise;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class MyCustomDialog extends DialogFragment {
+    private static final String TAG = "MyCustomDialog";
+    public OnInputListener mListener;
+
+    //widgets
+    private EditText mInput;
+    private TextView mActionOk, mActionCancel;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_my_custom, container, false);
+
+        mActionCancel = view.findViewById(R.id.action_cancel);
+        mActionOk = view.findViewById(R.id.action_ok);
+        mInput = view.findViewById(R.id.input);
+
+        mActionCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: closing dialog");
+                dismiss();
+            }
+        });
+
+
+        mActionOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: capturing input");
+
+                String input = mInput.getText().toString();
+
+                //"Best Practice" but it takes longer
+                mListener.sendInput(input);
+
+                dismiss();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public int getTheme() {
+        return R.style.CustomDialogTheme;
+    }
+
+    public interface OnInputListener {
+        void sendInput(String input);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof OnInputListener) {
+            mListener = (OnInputListener) context;
+        } else {
+            throw new RuntimeException(context + " must implement OnInputListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+}
